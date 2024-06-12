d3f01aca3ce051e3344d964a737e6035c14e36a5
AryRosvall <aryrosvall@AryRosvall-2.local>
2024-06-12 16:00:09 -0700

    Added login component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WelcomeComponent } from "./welcome/welcome.component";
+import { LoginComponent } from "./login/login.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, WelcomeComponent, LoginComponent]
 })
 export class AppComponent {
   title = 'todo';
+  message = 'holi';
+
 }
